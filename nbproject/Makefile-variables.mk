06b7da3ec44d10091c0f3b7802db88c913395b29
moh-samer <msamer340@gmail.com>
2021-09-25 10:48:31 +0200

    first update

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Alex_21_IO.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Alex_21_IO.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=alex21io.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/alex21io.x.tar
